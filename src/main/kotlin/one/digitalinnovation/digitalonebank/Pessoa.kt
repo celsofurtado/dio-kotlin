7827ad47d43962da9a3ac0a97c6613580f9fea71
Prof. Celso Furtado <celsofurtado@terra.com.br>
2021-02-16 15:11:21 -0300

    Versão 2 - 06-02-2021

--- src/main/kotlin/one/digitalinnovation/digitalonebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digitalonebank/Pessoa.kt
@@ -3,15 +3,22 @@ package one.digitalinnovation.digitalonebank
 class Pessoa {
 
     var nome: String = ""
-    var cpf: String = ""
+
+    var cpf: String = "895-965-541-77"
+    private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome -> $cpf"
 
 }
 
 fun main() {
     val celso = Pessoa()
     celso.nome = "Celso Furtado"
-    celso.cpf = "123.123.123-11"
+    //celso.cpf = "123.123.123-11"
 
+    println(celso.pessoaInfo())
     println(celso.nome)
     println(celso.cpf)
 }
